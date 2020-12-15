092ff5c07732fed511c0a9826e261df9f3d8ee25
mbayer2303 <mbayer2303@gmail.com>
2020-12-15 09:12:06 +0100

    Removed static/css & static/js

--- public/static/js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelectorAll(".menu-toggle").forEach(function (element) {
-    element.addEventListener("click", function(menuToggle) {
-      menuToggle.target.classList.toggle("active");
-      document.getElementById("main-menu").classList.toggle("active");
-    });
-  });
-});
