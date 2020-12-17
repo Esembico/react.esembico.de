9ad7bc7be08aaa761d2a3625b0169d0b23fd9d6e
mbayer2303 <mbayer2303@gmail.com>
2020-12-17 19:33:14 +0100

    Added a button to copy the code to CodeHighligher.

--- src/components/CodeHighligher.js
+++ src/components/CodeHighligher.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import { highlight } from "../highlighter";
 
 export default function CodeHighligher({ children, language }) {
@@ -8,5 +10,16 @@ export default function CodeHighligher({ children, language }) {
       setNewCode(highlight(children, lang.default));
     });
   }, [children, language]);
-  return <pre dangerouslySetInnerHTML={{ __html: newCode }}></pre>;
+
+  function copyCode() {
+    navigator.clipboard.writeText(children);
+  }
+  return (
+    <div style={{ position: "relative" }}>
+      <pre dangerouslySetInnerHTML={{ __html: newCode }}></pre>
+      <button title="Copy" className="float" onClick={copyCode}>
+        <FontAwesomeIcon icon={faCopy} />
+      </button>
+    </div>
+  );
 }
