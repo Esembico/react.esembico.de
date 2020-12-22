9ac7f97d476363bd04014d223b76b0ac548343e3
mbayer2303 <mbayer2303@gmail.com>
2020-12-22 18:38:47 +0100

    Fixed unused variable in Prototypes.js

--- src/pages/Prototypes.js
+++ src/pages/Prototypes.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Prototype from "../components/Prototype";
 
 export default function Prototypes() {
